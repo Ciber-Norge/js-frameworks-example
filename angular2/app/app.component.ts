7a36980673e3b5c0fab2375aed3217e8bd0aa1d0
Bjørn-Vegard Thoresen <bvst89@gmail.com>
2016-10-22 15:12:58 +0200

    Added example for two way binding with angular2

---
+++ angular2/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/app.component.html'
+})
+
+export class AppComponent { 
+    firstName = 'Kalle';
+    lastName = 'Klovn';
+    get fullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+}
